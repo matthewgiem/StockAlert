d38676b1a5f59c4024dfa1afb91be24753358914
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-09-26 14:50:03 +0700

    validate api key

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from api_keys import stock
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+print(stock)
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
