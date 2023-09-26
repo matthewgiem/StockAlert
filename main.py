efa71688faa6e4d13e818226d29a20d954fc46e9
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-09-26 15:22:10 +0700

    get the closing price for the day before yesterday

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ params = {
 r = requests.get(STOCK_ENDPOINT, params)
 data = r.json()
 
-# print(data["Time Series (Daily)"]["2023-09-25"].keys())
+print(data["Time Series (Daily)"].keys())
 print(data["Time Series (Daily)"]["2023-09-25"]["4. close"])
 
 #TODO 2. - Get the day before yesterday's closing stock price
+
+print(data["Time Series (Daily)"]["2023-09-22"]["4. close"])
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 
